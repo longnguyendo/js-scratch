0252aa4b2d4bdd0149538dc869d1b16f38d6e6a3
Long Be <longnguyen.open@gmail.com>
2025-06-08 23:41:40 +0300

    DOM event, try more event, and DOM

--- F8Js/HTMLDOM.js
+++ F8Js/HTMLDOM.js
@@ -66,7 +66,7 @@ productsElement.forEach((product) => {
 // firstProductElement.className = 'heading'; // setter;
 
 const a = document.querySelector('a');
-a.href = 'fullstack.edu.vn';
+// a.href = 'fullstack.edu.vn';
 // innerHTML --> Overwrite inside tag, and outnerHMTL overwrite the tag
 
 const box = document.querySelector('.box');
@@ -94,19 +94,138 @@ console.log("course", render(courses));
 // doc qua tung method, cua element, vi du nhu contenteditawble="";
 // --> nghia la co the edit khi nhan vao trong the day,(ke ca con no);
 
-let boxElement = document.querySelector('.box');
-console.log([boxElement]);
-// log ra cac properties node va prototype, sau nay co the se su dung
+// let boxElement = document.querySelector('.box');
+// console.log([boxElement]);
+// // log ra cac properties node va prototype, sau nay co the se su dung
  
-// DOM CSs
-console.log(boxElement.style); // will log properties of style to use it;
-// style = object;
-// we can use Object.assign(boxEle)  / gan 
-Object.assign(boxElement.style, {
-    width: '200px',
-    height: '100px',
+// // DOM CSs
+// console.log(boxElement.style); // will log properties of style to use it;
+// // style = object;
+// // we can use Object.assign(boxEle)  / gan 
+// Object.assign(boxElement.style, {
+//     width: '200px',
+//     height: '100px',
+// })
+// boxElement.style.color = 'red';
+
+// 148 classlist property
+// add
+// contain
+// remove
+// toggle
+
+// console.log(boxElement.classList); // so luong class
+
+// 154 dom events;
+// const button = document.querySelector('button');
+
+// button.onclick = function(e) {
+//     console.log('abc', e);
+//     button.style.backgroundColor='#fff';
+//     // button.classList.toggle('#fff')
+// }
+
+// 157 input / select 
+// key up / down 
+
+// const inputElement = document.querySelector('input[type="text"]');
+// console.log(inputElement);
+
+// onchange , oninput,  onkeyup, onkeydown, onkeypress (esc not in this method),
+// which is code of char on keyboard;
+// inputElement.onkeyup = function(e) {
+//     console.log(e.which);
+
+//     switch(e.which) {
+//         case 27: 
+//             console.log("exit");
+//             break;
+//         case 13:
+//             console.log("enter");
+//             break;
+//     }
+// }
+
+// inputElement.addEventListener('copy', (e)=> {
+//     alert('copy is not allowed');
+//     e.preventDefault();
+//     const selectText = window.getSelection().toString();
+//     console.log("you copied", selectText);
+    
+// })
+// block copy paste
+
+document.addEventListener('copy', (e) => {
+    // alert('copy is not allowed here');
+    e.preventDefault();
+    console.log("user copied smt!");
+
+    const selectText = window.getSelection().toString();
+    console.log('text copied', selectText);
+    
 })
-boxElement.style.color = 'red';
 
-// classlist property
+document.addEventListener('paste', (e) => {
+    // alert('copy is not allowed here');
+    e.preventDefault();
+    console.log("user copied smt!");
 
+    const selectText = window.getSelection().toString();
+    console.log('text copied', selectText);
+    
+})
+
+// 158 PreventDefault / stopPropagation
+// preventDefault hanh vi mat dinh cua site;
+// stop su kien noi bot // stop bubble behavior 
+// anchors - get a tag when they have name attribute;
+// document.links; --> html  Collection;
+// presearch log 
+// prevent default ngan chan moi hanh vi mat dinh ma no tao ra trong html
+
+// preventDefault -> tra ve hanh vi mat dinh cua site
+let links = document.links;
+for(let i = 0 ;  i < links.length; i ++) {
+    let link = links[i];
+    link.onclick = function(e) {
+        if (!e.target.href.startsWith('https://google.com')) {
+            e.preventDefault();
+        }
+    }  
+}
+let ulElement = document.querySelector('ul');
+
+ulElement.onmousedown = (e) => {
+    e.preventDefault();
+}
+
+ulElement.onclick = (e) => {
+    console.log(e.target);
+}
+// stopPropagation -> chan noi bot 
+stopDivEle = document.querySelector('.divStopPropagation');
+stopBoxEle = document.querySelector('.stopPropagation');
+
+stopDivEle.onclick=(e) => {
+    console.log('DIV');
+}
+stopBoxEle.onclick=(e)=> {
+    e.stopPropagation();
+    console.log('Box');
+}
+
+// 159 EvenListener
+// 1.Even Listener
+// 2.JSON
+// 3.Fetch
+// 4.DOM location
+// 5.Local storage
+// 6.Session storage
+// 7.Coding convention
+// 8.Best Practices
+// 9.Mistakes
+// 10.Performance;
+
+// 1. handle many task after 1 event happen
+// 2. Listen/ cancel listening
+// example, u can ask user that do u want to reload page
